632ca6d7212d6d433d8deb10a62d0b7d8be4e568
Ryans563 <r1294869@gmail.com>
2025-02-15 19:24:01 -0300

    Add files via upload

---
+++ abascr.js
@@ -0,0 +1,4 @@
+document.getElementById('continueBtn').addEventListener('click', function() {
+    document.querySelector('.container').style.display = 'none';
+    document.getElementById('biografias').classList.remove('hidden');
+});
